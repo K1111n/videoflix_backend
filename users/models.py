3da38ed7ba02cc7a4f2d644d22b303e7bdfedc53
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-04-23 07:52:01 +0200

    feat: add user model, registration endpoint and activation email via SMTP

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Email is required')
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import RegisterView, ActivateAccountView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('activate/<str:uidb64>/<str:token>/', ActivateAccountView.as_view(), name='activate'),
+]
