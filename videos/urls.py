1c774cb701493b059086986716bd5f9663a68dc2
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-04-24 16:23:53 +0200

    feat: add HLS video conversion with FFMPEG and streaming endpoints

--- videos/urls.py
+++ videos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import VideoListView
+from .views import VideoListView, HlsPlaylistView, HlsSegmentView
 
 urlpatterns = [
     path('video/', VideoListView.as_view(), name='video_list'),
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8', HlsPlaylistView.as_view(), name='hls_playlist'),
+    path('video/<int:movie_id>/<str:resolution>/<str:segment>/', HlsSegmentView.as_view(), name='hls_segment'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+from django.conf import settings
+from django.http import FileResponse, Http404
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .models import Video
 from .serializers import VideoSerializer
 
@@ -6,3 +12,27 @@ from .serializers import VideoSerializer
 class VideoListView(ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+
+class HlsPlaylistView(APIView):
+    def get(self, request, movie_id, resolution):
+        playlist_path = self._get_file_path(movie_id, resolution, 'index.m3u8')
+        return FileResponse(open(playlist_path, 'rb'), content_type='application/vnd.apple.mpegurl')
+
+    def _get_file_path(self, movie_id, resolution, filename):
+        path = Path(settings.MEDIA_ROOT) / 'videos' / 'hls' / str(movie_id) / resolution / filename
+        if not path.exists():
+            raise Http404
+        return path
+
+
+class HlsSegmentView(APIView):
+    def get(self, request, movie_id, resolution, segment):
+        segment_path = self._get_file_path(movie_id, resolution, segment)
+        return FileResponse(open(segment_path, 'rb'), content_type='video/MP2T')
+
+    def _get_file_path(self, movie_id, resolution, filename):
+        path = Path(settings.MEDIA_ROOT) / 'videos' / 'hls' / str(movie_id) / resolution / filename
+        if not path.exists():
+            raise Http404
+        return path
