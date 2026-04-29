f3b46d58f95c82808dc2d0b6d01ac668a1b62d4d
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-04-29 12:17:51 +0200

    docs: add docstrings to authentication and serializer classes

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,12 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    """Validate and create a new inactive user from email and password."""
+
     confirmed_password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -13,6 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
+        """Ensure passwords match and the email is not already registered."""
         if data['password'] != data['confirmed_password']:
             raise serializers.ValidationError('Passwords do not match.')
         if User.objects.filter(email=data['email']).exists():
@@ -20,20 +23,26 @@ class RegisterSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
+        """Create and return a new user, removing the confirmation field first."""
         validated_data.pop('confirmed_password')
         return User.objects.create_user(**validated_data)
 
 
 class PasswordConfirmSerializer(serializers.Serializer):
+    """Validate that new_password and confirm_password match."""
+
     new_password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
 
     def validate(self, data):
+        """Raise a validation error if both password fields are not identical."""
         if data['new_password'] != data['confirm_password']:
             raise serializers.ValidationError('Passwords do not match.')
         return data
 
 
 class LoginSerializer(serializers.Serializer):
+    """Deserialize and validate email and password for login."""
+
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
--- videos/serializers.py
+++ videos/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+
 from .models import Video
 
 
 class VideoSerializer(serializers.ModelSerializer):
+    """Serialize video data including an absolute thumbnail URL."""
+
     thumbnail_url = serializers.SerializerMethodField()
 
     class Meta:
@@ -10,6 +13,7 @@ class VideoSerializer(serializers.ModelSerializer):
         fields = ['id', 'created_at', 'title', 'description', 'category', 'thumbnail_url']
 
     def get_thumbnail_url(self, obj):
+        """Return the absolute URL of the thumbnail, or None if not set."""
         request = self.context.get('request')
         if obj.thumbnail and request:
             return request.build_absolute_uri(obj.thumbnail.url)
